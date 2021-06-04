bbc1039c276ebbb9ea0c57e95218a46fa6dab597
Architlall <archit10dgp@gmail.com>
2021-06-05 05:18:48 +0530

    meh

--- lib/playlist.dart
+++ lib/playlist.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart';
+
 import 'main.dart';
 import 'package:flutter/material.dart';
 import 'testplayer.dart';
@@ -6,8 +8,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class Playlist extends StatefulWidget {
   final String passedTitle;
-
-  const Playlist({Key key, this.passedTitle}) : super(key: key);
+  final String documentId;
+  const Playlist({Key key, this.passedTitle, this.documentId})
+      : super(key: key);
   @override
   _PlaylistState createState() => _PlaylistState();
 }
@@ -15,9 +18,10 @@ class Playlist extends StatefulWidget {
 class _PlaylistState extends State<Playlist> {
   Widget build(BuildContext context) {
     FirebaseAuth auth = FirebaseAuth.instance;
-
     CollectionReference collectionReference =
-        FirebaseFirestore.instance.collection('playlist');
+        FirebaseFirestore.instance.collection(auth.currentUser.uid);
+
+    final String id = auth.currentUser.uid;
 
     return Scaffold(
       appBar: AppBar(
@@ -27,31 +31,50 @@ class _PlaylistState extends State<Playlist> {
         Expanded(
             child: StreamBuilder(
           // stream: collectionReference.snapshots(),
-          stream: FirebaseFirestore.instance
-              .collection(auth.currentUser.uid)
-              .snapshots(),
+          stream: collectionReference.snapshots(),
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasData) {
-              return ListView(
-                children: snapshot.data.docs
-                    .map((e) => Column(
-                          children: [
-                            ListTile(
-                              title: Text(e['Name']),
-                            ),
-                            Divider(
-                              color: Colors.black.withOpacity(0.6),
-                              thickness: 2,
-                            )
-                          ],
-                        ))
-                    .toList(),
-              );
+              return ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    var Doc = snapshot.data.docs[index];
+
+                    return ListTile(
+                      title: Text(
+                        Doc['Name'],
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          snapshot.data.docs[index].reference.delete();
+                        },
+                      ),
+                    );
+                  });
+
+              // return ListView(
+              //   children: snapshot.data.docs
+              //       .map((e) => Column(
+              //             children: [
+              //               ListTile(
+              //                 title: Text(e['Name']),
+              //               ),
+              //               IconButton(
+              //                 icon: Icon(Icons.delete),
+              //                 onPressed: () {
+              //                   getUsers();
+              //                 },
+              //               ),
+              //               Divider(
+              //                 color: Colors.black.withOpacity(0.6),
+              //                 thickness: 2,
+              //               )
+              //             ],
+              //           ))
+              //       .toList(),
+              // );
             }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
           },
         ))
       ]),
